074048329d4cfd4019fdbfdf7aa1d49883c7b077
yip-jm <64177370+yip-jm@users.noreply.github.com>
2022-01-20 23:07:33 +0800

    Add files via upload

---
+++ baselines/run_experiment.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from dopamine.discrete_domains import run_experiment
+import gin
+
+
+@gin.configurable
+class LoggedRunner(run_experiment.Runner):
+
+  def run_experiment(self):
+    super(LoggedRunner, self).run_experiment()
+    # Log the replay buffer at the end
+    self._agent.log_final_buffer()
